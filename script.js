d3c4884c88b5f4f59be9e687ca88a351ca8817b4
function07 <houssine199222@gmail.com>
2023-01-18 00:17:07 -0500

    Add object for gameBoard containing an empty array and empty objects for displayController and playerFactory

---
+++ script.js
@@ -0,0 +1,10 @@
+const gameBoard = (() => {
+  const gameBoardArray = [];
+  return { gameBoardArray };
+})();
+
+const displayController = (() => {})();
+
+const playerFactory = () => {
+  const displayName = () => {};
+};
