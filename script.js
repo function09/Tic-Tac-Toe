c9d426c66b330055841bb062d06155abc9127b37
function07 <houssine199222@gmail.com>
2023-01-25 00:21:58 -0500

    Add feature to playerFactory function that will allow players to click on a square and mark it

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const gameBoard = (() => {
-  const gameBoardArray = [""];
-
-  selectSquares.forEach((square) => {
-    square.addEventListener("click", () => {
-      console.log();
-    });
-  });
+  const gameBoardArray = ["", "", "", "", "", "", "", "", ""];
+  return { gameBoardArray };
 })();
 
-const playerFactory = () => {};
+const playerFactory = (name, marker) => {
+  const getName = name;
+  const getMarker = marker;
+  const selectSquares = document.querySelectorAll(".square");
+  const selectGameBoardArray = gameBoard.gameBoardArray;
+
+  const addMarker = selectSquares.forEach((square) => {
+    square.addEventListener("click", () => {
+      const selectIndex = Number(square.dataset.index);
+
+      if (selectGameBoardArray[selectIndex] === "") {
+        selectGameBoardArray.splice(selectIndex, 1, getMarker);
+      } else {
+        return;
+      }
+      square.textContent = selectGameBoardArray[selectIndex];
+    });
+  });
+  return {};
+};
 
 const gameFlow = (() => {})();
