38b882d1e340cfd584134465a84fcef4bda77567
function07 <houssine199222@gmail.com>
2023-01-31 23:17:39 -0500

    Add feature that records who won

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const gameFlow = (() => {
   const player1 = playerFactory("X");
   const player2 = playerFactory("O");
   let currentPlayer = player2;
-
   const takeTurns = () => {
     if (currentPlayer === player2) {
       currentPlayer = player1;
@@ -23,6 +22,42 @@ const gameFlow = (() => {
     return currentPlayer.getMarker();
   };
   return { takeTurns };
+})();
+
+const winConditions = (() => {
+  const getGameBoardArray = gameBoard.gameBoardArray;
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const checkWin = () => {
+    const player1Winner = (element) => {
+      const player1Wins = getGameBoardArray[element] === "X";
+
+      return player1Wins;
+    };
+
+    const player2Winner = (element) => {
+      const player2Wins = getGameBoardArray[element] === "O";
+
+      return player2Wins;
+    };
+    for (let i = 0; i < winningCombinations.length; i++) {
+      if (winningCombinations[i].every(player1Winner)) {
+        console.log("Player1 wins!");
+      } else if (winningCombinations[i].every(player2Winner)) {
+        console.log("Player2 wins!");
+      }
+    }
+  };
+
+  return { checkWin };
 })();
 
 const getDOM = (() => {
@@ -38,6 +73,7 @@ const getDOM = (() => {
           return;
         }
         square.textContent = gameBoard.gameBoardArray[selectIndex];
+        winConditions.checkWin();
       });
     });
   };
